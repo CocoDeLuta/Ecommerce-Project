3bc064a1cac3bc3808b8eec16e05f53b0e960e50
JoaozinhoDrgnBr <116232109+JoaozinhoDrgnBr@users.noreply.github.com>
2023-06-13 14:14:47 -0300

    Add files via upload

---
+++ auth/auth.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once("lib/jwt.inc.php");
+require_once("config.php");
+
+class Auth {
+    private $pdo;
+
+    public function __construct() {
+        // Inicialize a conexão PDO aqui
+        $this->pdo = new PDO("mysql:host=localhost;dbname=mpbdb", "root", "");
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    public function login() {
+        $json = file_get_contents('php://input');
+        $credentials = json_decode($json);
+
+        $responseBody = '';
+
+        $stmt = $this->pdo->prepare("SELECT * FROM tb_usuario WHERE nome = :nome AND senha = :senha");
+        $stmt->bindValue(":nome", $credentials->nome);
+        $stmt->bindValue(":senha", $credentials->senha);
+        $stmt->execute();
+
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($user && $user['nome'] == $credentials->nome && $user['senha'] == $credentials->senha) {
+            $payload = [
+                "id" => $user['id'],
+                "username" => $user['nome'],
+                "role" => "admin"
+            ];
+
+            $token = JwtUtil::encode($payload, JWT_SECRET_KEY);
+
+            $responseBody = '{ "token": "'.$token.'" }';
+        } else {
+            http_response_code(401);
+            $responseBody = '{ "message": "Credencial inválida" }';
+        }
+
+        echo $responseBody;
+    }
+}
+
+$auth = new Auth();
+$auth->login();
+
+?>
---
+++ auth/config.php
@@ -0,0 +1,3 @@
+<?php
+define("JWT_SECRET_KEY", "nf1923isd09a31jkl42190s");
+?>
