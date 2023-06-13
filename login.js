805dc3030434a4d6656d718aaf79c3eddca12680
JoaozinhoDrgnBr <116232109+JoaozinhoDrgnBr@users.noreply.github.com>
2023-06-13 14:16:07 -0300

    prototipo login

---
+++ login.js
@@ -0,0 +1,34 @@
+const URL = "http://localhost/Sobjucas";
+
+function log() {
+  const fNome = document.getElementById("fNome");
+  const fSenha = document.getElementById("fSenha");
+
+  const pessoa = {
+    nome: fNome.value,
+    senha: fSenha.value
+  };
+
+  // Cliente HTTP faz a requisição para a API
+  fetch(`${URL}/auth/auth.php`, {
+    body: JSON.stringify(pessoa),
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+  }).then((res) => {
+    if (res.status === 200 || res.status === 201) {
+      alert("Logado com sucesso");
+
+      res.json().then((data) => {
+        addTableRow(data);
+      });
+    } else if (res.status === 401) {
+      res.json().then((error) => {
+        alert(error.message);
+      });
+    } else {
+      alert("Ocorreu um erro na autenticação");
+    }
+  });
+}
